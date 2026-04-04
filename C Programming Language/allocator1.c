712457525ab0a27b149e10c8fd405a993678785a
alessandbss <163196470+alessandbss@users.noreply.github.com>
2026-04-04 13:51:49 -0300

    Add files via upload

--- C Programming Language/allocator1.c
+++ C Programming Language/allocator1.c
@@ -1,12 +1,26 @@
 char galpao[10000];
 int marcador_global = 0;
 
-int alocar_espaco(int quantidade) {
-	var int marcador_temp = marcador_global;
-	marcador_global += quantidade;
-	
-	if (marcador_temp + quantidade > 10000):
-		return -1;
-	else:
-		return marcador_temp;	
+void* alocar_espaco(int quantidade) {
+	int marcador_temp = marcador_global;
+		
+	if (marcador_temp + quantidade + sizeof(int) > 10000) {
+		return NULL;
+	} else {
+		marcador_global += quantidade + sizeof(int);
+		
+		void* piso_atual = galpao + marcador_temp;
+		int* esconderijo = piso_atual;
+		esconderijo[0] = quantidade;
+		
+		void* piso_alocado = esconderijo + 1;
+		return piso_alocado;	
+	}
 }
+
+void limpar_galpao() {
+	marcador_global = 0;
+}
+
+void* meu_terreno = alocar_espaco(150);
+char* texto = meu_terreno;
